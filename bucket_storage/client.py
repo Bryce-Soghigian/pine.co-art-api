c1bc74e831bdd3fdb3609d41a5d91cd49ca7da6d
bryce-soghigian <bsoghigian@gmail.com>
2022-04-09 21:13:34 -0700

    working compose

--- bucket_storage/client.py
+++ bucket_storage/client.py
@@ -1,13 +1,28 @@
+import typing
 import os
+import urllib3
 from common.utils import verify_env_vars
 from minio import Minio
+from urllib3.exceptions import MaxRetryError
 
 
-def get_client():
+def get_client() -> typing.Union[Minio, None]:
     verify_env_vars(['MINIO_HOST', 'MINIO_ACCESS_KEY', 'MINIO_SECRET_KEY'])
-    return Minio(
-        os.getenv('MINIO_HOST'),
-        access_key=os.getenv('MINIO_ACCESS_KEY'),
-        secret_key=os.getenv('MINIO_SECRET_KEY'),
-        secure=False,
-    )
+    url = f"http://{os.getenv('MINIO_HOST')}"
+    config = {
+        "endpoint": "localhost:9000",
+        "access_key": os.getenv('MINIO_ACCESS_KEY'),
+        "secret_key": os.getenv("MINIO_SECRET_KEY"),
+        "secure": False,
+        "http_client": urllib3.PoolManager(
+            num_pools=10,
+        )
+    }
+    try:
+        client = Minio(**config)
+        print(dir(client))
+        client.list_buckets()
+        print(client)
+        return client
+    except MaxRetryError:
+        print("Object Storage Not Reachable.")
